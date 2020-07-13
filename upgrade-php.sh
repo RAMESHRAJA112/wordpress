7006ee91050651b90446017c56ed9e6fe912cdd7
Shreenivas Iyer <shreenivas.i@gmail.com>
2020-07-13 00:53:57 -0400

    updated upgrade script

--- upgrade-php.sh
+++ upgrade-php.sh
@@ -1,5 +1,5 @@
 rpm -Uvh https://mirror.webtatic.com/yum/el7/epel-release.rpm
-                rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
-                yum remove php-common
-                yum install -y php56w php56w-opcache php56w-xml php56w-mcrypt php56w-gd php56w-devel php56w-mysql php56w-intl php56w-mbstring --skip-broken
-                service httpd restart
+rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
+yum remove -y php-common
+yum install -y php56w php56w-opcache php56w-xml php56w-mcrypt php56w-gd php56w-devel php56w-mysql php56w-intl php56w-mbstring --skip-broken
+service httpd restart
